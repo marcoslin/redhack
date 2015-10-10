8011b4e381210fa9960cf34697cc55e80141d551
Marcos Lin <linm.macpro@ebacimail.com>
2015-10-11 01:10:04 +0200

    Improved graphic nav

--- app/www/scripts/ctrls.js
+++ app/www/scripts/ctrls.js
@@ -1,6 +1,22 @@
 (function () {
     var app = angular.module('redhack');
 
+    app.controller('CoreController', ['$scope', '$rootScope', '$log', function ($scope, $rootScope, $log) {
+        $scope.fs = {
+            'show_logo': false,
+            'row_style': 'col-xs-10 col-xs-offset-1'
+        };
+
+        $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+            $log.info('State changed to', toState);
+            if (toState.name === 'home') {
+                $scope.fs.show_logo = false;
+            } else {
+                $scope.fs.show_logo = true;
+            }
+        });
+    }]);
+
 
     app.controller('HomeController', ['$scope', '$log', function ($scope, $log) {
         $scope.message = 'hello';
