4e48ad448df503e2e18e36c7daff557e294b40f5
melvinm99 <massotti.melvin@gmail.com>
2015-10-11 12:47:40 +0200

    commit di melvin

---
+++ Query.py
@@ -0,0 +1,133 @@
+__author__ = 'mel'
+import xml.etree.ElementTree  as ET
+from flask import Flask
+from flask import request
+from flask import jsonify
+import urllib2 as url
+import json
+import time
+import datetime
+tree = ET.parse('/Users/mel/Desktop/Kismet-20151010-15-04-59-1.xml')
+e =  tree.getroot()
+set = set()
+for atype in e.iter('client-mac'):
+    set.add(atype.text)
+
+
+app = Flask(__name__)
+
+@app.route("/numeropersone")
+def trovapersone():
+    return str(len(set))
+
+@app.route("/trovastazione")
+def trovaStazione():
+    results = []
+    name = request.args.get("name")
+    try:
+        response = url.urlopen("http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/autocompletaStazione/"+url.quote(name))
+    except Exception, ex:
+        print(ex)
+
+    for line in response.read().rstrip().split("\n"):
+        arr = line.split("|")
+        results.append({"name":str(arr[0]),"id":str(arr[1])})
+    return jsonify({"results":results})
+
+tempoModificato=12
+orarioArrivoInStazione=0
+
+@app.route("/setdelay")
+def setdelay():
+    global tempoModificato
+    tempoModificato = 900
+    return True
+
+
+def controllopartenza(orig,dest,time):
+    #ottengo id del treno in partenza
+    try:
+        uerrelle = "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/soluzioniViaggioNew/"+orig[2:]+"/"+dest[2:]+"/"+time
+        response = url.urlopen(uerrelle).read()
+    except Exception, ex:
+        print(ex)
+    ###########
+    data = json.loads(response)
+    idTreno=""
+    for prop in data["soluzioni"]:
+        for property in prop["vehicles"]:
+            if property["orarioPartenza"]==time:
+                idTreno=property["numeroTreno"]
+                break
+    try:
+        uerrelle = "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/andamentoTreno/"+orig+"/"+idTreno
+        response = url.urlopen(uerrelle).read()
+    except Exception, ex:
+        print(ex)
+    data = json.loads(response)
+    orarioPartenza=0
+    property = data["fermate"][0]
+    orarioPartenza = property["partenza_teorica"]
+    orarioPartenza+=tempoModificato                     #hardcode sul tempo di partenza
+    orarioPartenzaDatetime = datetime.datetime.fromtimestamp(orarioPartenza)
+
+    if orarioPartenza>orarioArrivoInStazione:
+        return False
+    return True
+
+@app.route("/ritornasoluzionediviaggio")    #ritorna una soluzione di viaggio alternativa, (la successiva ), se in ritardo
+def ritornaitinerario():
+    response = ""
+    orig = request.args.get("origine")
+    dest = request.args.get("destinazione")
+    time = request.args.get("orario")
+    bool = controllopartenza(orig,dest,time)
+    if bool:
+        return "ok"
+    #cerco alternativa
+    try:
+        uerrelle = "http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/soluzioniViaggioNew/"+orig[2:]+"/"+dest[2:]+"/"+time
+        response = url.urlopen(uerrelle).read()
+    except Exception, ex:
+        print(ex)
+
+    data = json.loads(response)
+    proposta = data["soluzioni"][0]
+    return jsonify({"soluzione_alternativa_viaggio":proposta})
+
+
+@app.route("/controlloitinerario")              #controllo sullo scambio
+def controlloitinerario():
+    results = []
+    trenoOrig = request.args.get("trenodiorigine")
+    stazioneCambio = request.args.get("stazionedicambio")
+    trenoDest = request.args.get("trenodaprendere")
+    orario  = getT1(stazioneCambio, trenoOrig)
+
+    try:
+        response = url.urlopen("http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/soluzioniViaggioNew/"+orig+"/"+dest+"/"+time)
+    except Exception, ex:
+        print(ex)
+
+    return True
+
+def getT1(stationId, trainId):
+    try:
+        response = url.urlopen("http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/andamentoTreno/"+stationId+"/"+trainId)
+    except Exception, ex:
+        print(ex)
+    t1 = None
+    var = response.read()
+    data = json.loads(var)
+    for stop in data["fermate"]:
+        if stop["id"] == str(stationId):
+            t1 = datetime.fromtimestamp(time.gmtime(stop["partenza_teorica"]))
+            timedelta = datetime.timedelta(minutes=stop["ritardo"])
+    return t1+timedelta
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='10.10.1.40') #host='10.10.1.40'
